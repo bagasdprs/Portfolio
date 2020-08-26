e7674f3cc83eeb436717682da32847f5ee1f2ab9
bagasdprs <bagasdprs@gmail.com>
2020-08-26 19:39:30 +0700

    My Portfolio

---
+++ js/main.js
@@ -0,0 +1,15 @@
+alert('Welcome to my profile');
+
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+                scrollTop: $(hash).offset().top - 80
+            },
+            800
+        );
+    }
+});
